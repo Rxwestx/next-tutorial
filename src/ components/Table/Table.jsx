f381f5109e679921d09c29c2c2ac5ddc70468281
Rxwestx <wexriexst13@gmail.com>
2026-06-29 02:01:17 +0900

    add: テーブルコンポーネントにダミーデータを追加し、動的に表示するように変更

--- src/ components/Table/Table.jsx
+++ src/ components/Table/Table.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import styles from './Table.module.css' 
+
+const dummyData = [
+    {
+        date: "2024-06-01",
+        name: "John Doe",
+        task: "Task 1",
+        comment: "Comment 1",
+    },
+    {
+        date: "2024-06-02",
+        name: "John Doe",
+        task: "Task 1",
+        comment: "Comment 2",
+    },
+    {
+        date: "2024-06-03",
+        name: "John Doe",
+        task: "Task 1",
+        comment: "Comment 3",
+    },
+];
+
 export default function Table() {
   return (
     <table className={styles.table}>
+        
         <thead className={styles.tableHead}>
             <tr>
                 <th>Date</th>
@@ -12,12 +35,14 @@ export default function Table() {
             </tr>
         </thead>
         <tbody className={styles.tableBody}>
-            <tr>
-                <td>2024-06-01</td>
-                <td>John Doe</td>
-                <td>Task 1</td>
-                <td>Comment 1</td>
-            </tr>
+            {dummyData.map(item => ( 
+                <tr >
+                    <td>{item.date}</td>
+                    <td>{item.name}</td>
+                    <td>{item.task}</td>
+                    <td>{item.comment}</td>
+                </tr>
+            ))}
         </tbody>
     </table>
   )
